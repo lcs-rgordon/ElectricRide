0b0c6055383ce3304fb8b07fb5e888c706f6cd20
Russell Gordon <rgordon@lcs.on.ca>
2025-05-30 14:48:39 -0400

    Initial Commit

---
+++ ElectricRide/ElectricRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectricRideApp.swift
+//  ElectricRide
+//
+//  Created by Russell Gordon on 2025-05-30.
+//
+
+import SwiftUI
+
+@main
+struct ElectricRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
