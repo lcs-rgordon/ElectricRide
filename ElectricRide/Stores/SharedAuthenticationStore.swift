0fd2120f152b91b1fefd458d5d5c67e23d658b1f
Russell Gordon <rgordon@lcs.on.ca>
2025-06-02 06:53:21 -0400

    Removed simulated sign-in code.

--- ElectricRide/Stores/SharedAuthenticationStore.swift
+++ ElectricRide/Stores/SharedAuthenticationStore.swift
@@ -16,8 +16,6 @@ class SharedAuthenticationStore {
     // The currently signed in patron
     var signedInPatron: Patron?
     
-    // List of all patrons using the app
-    var patrons: [Patron] = []
 
     // MARK: Computed properties
     var patronIsSignedIn: Bool {
@@ -34,36 +32,9 @@ class SharedAuthenticationStore {
         // To begin, there is no one signed in
         self.signedInPatron = nil
         
-        // Actually get the list of patrons
-        Task {
-            try await self.getAllPatrons()
-        }
     }
     
     // MARK: Function(s)
-    func getAllPatrons() async throws {
-
-        Logger.database.info("SharedAuthenticationStore: About to retrieve all patrons.")
-
-        do {
-            
-            let results: [Patron] = try await supabase
-                .from("patron")
-                .select()
-                .execute()
-                .value
-
-            Logger.database.info("SharedAuthenticationStore: Patrons retrieved; about to assign results to `patrons` array.")
-
-            self.patrons = results
-            
-        } catch {
-            Logger.database.error("SharedAuthenticationStore: Could not load available patrons.")
-            Logger.database.error("\(error)")
-        }
-        
-    }
-    
     func setSignedInPatron(to selectedPatron: Patron) {
         
         self.signedInPatron = selectedPatron
